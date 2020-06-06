c99ae12f67c4be288cf41cdaa0147ce662f3b41c
Shruti <tirpude.shruti12@gmail.com>
2020-06-05 19:17:16 -0500

    added React frontend

--- CheckMailExcercise/ReactUICheckMail/checkemail/src/UserPost.js
+++ CheckMailExcercise/ReactUICheckMail/checkemail/src/UserPost.js
@@ -8,21 +8,33 @@ class UserPost extends Component {
 
 		this.state = {
 			
-			email: ''
-			
+			email: '',
+            myObject: {data:0}
+           // myObject: {}
+           //data : null
+
+            
 		}
+
+        this.changeHandler = this.changeHandler.bind(this);
+
     }
 
     
     changeHandler = (e) => {
         this.setState({[e.target.name]: e.target.value})
+        //this.myObject = this.myObject.bind(this);
+         //return {data: this.state.data};
     }
 
-    submitHandler = (e) =>{
+       submitHandler = (e) =>{
         e.preventDefault()
         console.log(this.state)
-        axios.post('http://localhost:8080/checkemail',this.state)
+        axios.post('http://ec2-52-35-138-206.us-west-2.compute.amazonaws.com:8080/checkemail',this.state)
         .then(response =>{
+            this.state.myObject.data=response.data;
+            //this.setState({myObject})
+            console.log("5555555"+this.state.myObject.data)
             console.log(response)
         })
         .catch(error => {
@@ -31,14 +43,18 @@ class UserPost extends Component {
 
     }
     render() { 
-        const { emails } = this.state
+        const { emails,myObject } = this.state
         return ( 
             <form onSubmit={this.submitHandler}>
                 <div>
                 <input type="text" name="emails" value={emails} onChange={this.changeHandler}></input>
                 </div>
-                <button type="submit">Check Email</button>
-            </form>
+                <button type="submit">Check Email 11111</button>
+                <text>THis is retrun value: </text>
+                 
+                <span>{this.state.myObject.data}</span>
+                </form >
+            
          );
     }
 }
